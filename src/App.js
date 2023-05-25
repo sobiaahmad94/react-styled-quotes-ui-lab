6c59ddc6f81cd006cc04e03038fc7516bd40f9b3
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-05-25 15:44:26 +0100

    almost done

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Quote from './components/Quote';
 function App() {
   return (
     <div className="App">
-      <Quote by="-Bill Gates" source="https://www.microsoft.com">
+      <Quote by="Bill Gates" source="https://www.microsoft.com">
         640kb of memory ought to be enough for anyone
       </Quote>
-      <Quote by="-Steve Jobs" source="https://www.apple.com">
+      <Quote by="Steve Jobs" source="https://www.apple.com">
         Don't let the noise of others' opinions drown out your own inner voice
       </Quote>
       {/* <Quote /> has been added 3 times*/}
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,13 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const QuotesWithStyling = styled.figure`
-        color: blue;
+        font-family: Arial, Helvetica, sans-serif; 
+        color: rgb(92, 89, 89);
+        font-size: 10x;
+        background-color: rgb(218, 216, 216);
         border-radius: 25px;
-        border: 2px solid #73AD21;
         padding: 20px;
-        width: 200px;
+        width: 400px;
         height: 150px;
 `
+// const BlockQuotesStyling
+//     `
 
 // need to pass props in as parameters Quote(props) - props.by props.source, props.children
 // use destructuring to make it neater
@@ -20,10 +24,9 @@ function Quote(props) {
     return (
         <>
         <figure>
-
             <QuotesWithStyling>
                     <cite>"{props.children}"</cite>
-                    <p>{props.by}</p>
+                    <blockquote className="quote-author">-{props.by}</blockquote>
                     <p>Source: <a href={props.source}>{props.source}</a></p>
             </QuotesWithStyling>
         </figure>
